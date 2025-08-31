6a386b73cd381ec84426dba18529dee23f40cd89
James-Lian <166276178+James-Lian@users.noreply.github.com>
2025-08-30 22:52:31 -0400

    Add files via upload

--- api/githubInfo.js
+++ api/githubInfo.js
@@ -41,6 +41,6 @@ export default async function handler(req, res) {
         
         console.log(files);
 
-        res.status(200).json({ content: files });
+        res.status(200).json({ data: files });
     }
 }
